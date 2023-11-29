fc9b9d60a2a777c6539a76d77bb003a8604a349f
Daniel Eriksson <daern91@gmail.com>
2023-11-28 21:22:09 -0500

    progress

--- src/main.rs
+++ src/main.rs
@@ -28,19 +28,26 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buffer = [0; 1024]; // max size of 1024 bytes
-    if let Ok(_) = stream.read(&mut buffer) {
-        let request = String::from_utf8_lossy(&buffer[..]);
-        request.lines().for_each(|line| println!("{}", line));
-        let mut lines = request.lines();
-        let first_line = lines.next().unwrap();
-        let path = first_line.split_whitespace().nth(1).unwrap();
-        if path == "/" {
-            let response = "HTTP/1.1 200 OK\r\n\r\n";
-            stream.write(response.as_bytes())?;
-        } else {
-            let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-            stream.write(response.as_bytes())?;
-        }
+    stream.read(&mut buffer)?;
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+    request.lines().for_each(|line| println!("{}", line));
+    let mut lines = request.lines();
+    let first_line = lines.next().unwrap();
+    let path = first_line.split_whitespace().nth(1).unwrap();
+
+    if path == "/" {
+        stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+    } else if path.starts_with("/echo/") {
+        let echo = path.split("/echo/").nth(1).unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            echo.len(),
+            echo
+        );
+        stream.write(response.as_bytes())?;
+    } else {
+        stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
     }
 
     Ok(())
